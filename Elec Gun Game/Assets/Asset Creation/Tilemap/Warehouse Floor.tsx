bc9028b85d1014a846940850557d7e0517845c06
Samiul Haque <mdsamiul.haque@ucalgary.ca>
2024-11-07 16:44:29 -0700

    sam tutorial maps

--- Elec Gun Game/Assets/Asset Creation/Tilemap/Warehouse Floor.tsx
+++ Elec Gun Game/Assets/Asset Creation/Tilemap/Warehouse Floor.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Warehouse Floor" tilewidth="64" tileheight="64" tilecount="11" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="Warehouse Floor" tilewidth="315" tileheight="250" tilecount="24" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="Floor.png" width="16" height="16"/>
@@ -51,5 +51,44 @@
   <objectgroup draworder="index" id="2">
    <object id="1" x="5.99296" y="18.9778" width="17.9432" height="1.09091"/>
   </objectgroup>
+ </tile>
+ <tile id="15">
+  <image source="BL.png" width="16" height="16"/>
+ </tile>
+ <tile id="16">
+  <image source="BM.png" width="16" height="16"/>
+ </tile>
+ <tile id="17">
+  <image source="BR.png" width="16" height="16"/>
+ </tile>
+ <tile id="18">
+  <image source="door1.png" width="16" height="32"/>
+ </tile>
+ <tile id="19">
+  <image source="Floor 1.png" width="16" height="16"/>
+ </tile>
+ <tile id="20">
+  <image source="Floor2 1.png" width="16" height="16"/>
+ </tile>
+ <tile id="21">
+  <image source="ML.png" width="16" height="16"/>
+ </tile>
+ <tile id="22">
+  <image source="MM.png" width="16" height="16"/>
+ </tile>
+ <tile id="23">
+  <image source="MR.png" width="16" height="16"/>
+ </tile>
+ <tile id="24">
+  <image source="TL.png" width="16" height="16"/>
+ </tile>
+ <tile id="25">
+  <image source="TM.png" width="16" height="16"/>
+ </tile>
+ <tile id="26">
+  <image source="TR.png" width="16" height="16"/>
+ </tile>
+ <tile id="27">
+  <image source="../../Level Design/Sam Tutorial Tiled/bird.png" width="315" height="250"/>
  </tile>
 </tileset>
---
+++ Elec Gun Game/Assets/Level Design/Sam Tutorial Tiled/bird.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="bird" tilewidth="32" tileheight="32" tilecount="63" columns="9">
+ <image source="bird.png" width="315" height="250"/>
+</tileset>
