8037d2445f34b2a58154e5cd6c1b58d89a686619
Cody Casselman <cody.casselman@ucalgary.ca>
2024-12-29 20:07:30 -0700

    Update DeathZone.cs

--- Elec Gun Game/Assets/DeathZone.cs
+++ Elec Gun Game/Assets/DeathZone.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DeathZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Destroy whatever is attached to the collider
-        Destroy(collision.gameObject);
+        if (collision.gameObject.GetType() == Player) { }
     }
 }
