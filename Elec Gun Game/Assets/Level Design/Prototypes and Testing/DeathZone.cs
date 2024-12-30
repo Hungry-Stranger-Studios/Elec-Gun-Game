c1ccb4ad6d8eb052b4ab70300e553466f83df7df
Cody Casselman <cody.casselman@ucalgary.ca>
2024-12-30 15:28:49 -0700

    (FEATURE) DeathZone scripting
    Added deathzone object which will kill the player when they enter it. Triggers respawning actions. TODO: Add functionality with a custom enemy controller script. Modify respawn such that it freezes player movement. (Lower priority)

---
+++ Elec Gun Game/Assets/Level Design/Prototypes and Testing/DeathZone.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class DeathZone : MonoBehaviour
+{
+    private Object player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If the object being killed is a player
+        if (collision.gameObject.Equals(player)) {
+            player.GetComponent<PlayerMovement>().KillPlayer();
+        }
+        else if (collision.gameObject.tag == "Enemy")
+        {
+            //TODO: Add enemy controller script with a kill command.
+        }
+    }
+}
