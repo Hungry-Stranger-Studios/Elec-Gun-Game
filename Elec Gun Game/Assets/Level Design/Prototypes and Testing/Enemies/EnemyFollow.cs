58fa603af8bfc14807191f551e6faac2e162d2bb
Samiul Haque <mdsamiul.haque@ucalgary.ca>
2025-01-07 18:22:08 -0700

    EnemyFollow script
    Made a simple script that could be applied generally to the enemy prefab (that doesnt currently yet exist for the level), Enemy logically follows player, BUT if the enemy encounters a rigid body with the tag "Wall" it experiences and upward force and jumps over the wall so that it can follow the enemy over specified obstacles.

---
+++ Elec Gun Game/Assets/Level Design/Prototypes and Testing/Enemies/EnemyFollow.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    private Transform target;
+    public float speed = 2f;
+    [SerializeField] private float jumpMult = 25f;
+    private bool isGrounded;
+
+    [SerializeField] private Rigidbody2D enemyRigidbody;
+    private float jumpCooldown = 0.2f;
+    private float nextJumpTime;
+
+    void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        enemyRigidbody = GetComponent<Rigidbody2D>();
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall") && isGrounded && Time.time >= nextJumpTime)
+        {
+            Debug.Log("Wall detected! Jumping...");
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        enemyRigidbody.AddForce(Vector2.up * 20, ForceMode2D.Impulse);
+        nextJumpTime = Time.time + jumpCooldown;
+    }
+}
